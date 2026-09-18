b48a690763cef0810dcf260a61416fb3805c3ae4
29thaynan1c <thaynan.silva@escola.pr.gov.br>
2026-09-18 08:20:00 -0300

    html

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ botoes.forEach(function (botao) {
     let curtiu = false;
     
     botao.addEventListener("click", botaoClicado);
-    
+
     function botaoClicado() {
         console.log("fui clicado");
         let texto = botao.querySelector("span");
@@ -17,16 +17,17 @@ botoes.forEach(function (botao) {
         }
     }
 });
- 
-const btnTemaEscuro  = document.querySelector(".btn-tema-escuro");
 
-btnTemaEscuro.addEventListener("click", muda-tema);
+const btnTemaEscuro = document.querySelector(".btn-tema-escuro");
+
+
+btnTemaEscuro.addEventListener("click", mudaTema);
 
 function mudaTema() {
     const corpoPagina = document.body;
     if (corpoPagina.classList.contains("tema-escuro")) {
-        corpoPagina.classList.remove("tema-escuro")
-    } else{
-        corpoPagina.classList.add("tema-escuro")
+        corpoPagina.classList.remove("tema-escuro");
+    } else {
+        corpoPagina.classList.add("tema-escuro");
     }
 }
